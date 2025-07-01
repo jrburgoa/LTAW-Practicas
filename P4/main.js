4343f26352f1f28d5fd58be64fd5c838787ddb2d
jrburgoa <jr.burgoa.2019@alumnos.urjc.es>
2025-07-02 00:37:04 +0200

    actualizacion del main

--- P4/main.js
+++ P4/main.js
@@ -1,144 +1,181 @@
-const socketServer = require('socket.io').Server;
+// main.js
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
 const http = require('http');
 const express = require('express');
-const fs = require('fs');
 const colors = require('colors');
-
+const ip = require('ip');
+const qrcode = require('qrcode');
+const socketServer = require('socket.io').Server;
 
 const PUERTO = 8080;
-const CHAT_HTML = fs.readFileSync('/P3/userChat.html', 'utf-8');
-let clients = []
-const app = express();
+const CHAT_HTML = fs.readFileSync(path.join(__dirname, 'P3/userChat.html'), 'utf-8');
 
-app.use('/', express.static(__dirname));
-app.use(express.static('publico'));
+let clients = [];
+let win = null;
 
-const server = http.Server(app);
-const io = new socketServer(server);
+// --- SERVIDOR EXPRESS Y SOCKET.IO ---
+const serverApp = express();
+serverApp.use('/', express.static(__dirname));
+serverApp.use(express.static('publico'));
 
-app.post('/login', (req,res) => {
-    let userName = ""
+serverApp.post('/login', (req, res) => {
     let data = '';
-    req.on('data', chunk => {
-        data += chunk;
-    });
+    req.on('data', chunk => { data += chunk; });
     req.on('end', () => {
         const datos = new URLSearchParams(data);
-        userName = datos.get('userName');
-        const clientsName = clients.map(objeto => objeto.name);
-        if(clientsName.includes(userName)){
-            res.status(404).send("Nombre de usuario ya en uso");
-        }else if(userName.toLowerCase() == "server" ){ 
-            res.status(404).send("Nombre de usuario no disponible");
-        }else if(userName == ""){
-            res.status(404).send("Necesitas un nombre de usuario, este campo no puede estar vacio");
-        }else{
-            res.send(CHAT_HTML);
-        }
+        const userName = datos.get('userName');
+        const names = clients.map(c => c.name);
+        if (!userName) return res.status(404).send("Necesitas un nombre de usuario");
+        if (names.includes(userName)) return res.status(404).send("Nombre de usuario ya en uso");
+        if (userName.toLowerCase() === 'server') return res.status(404).send("Nombre de usuario no disponible");
+        res.send(CHAT_HTML);
     });
 });
 
+const httpServer = http.createServer(serverApp);
+const io = new socketServer(httpServer);
+
 io.on('connect', (socket) => {
-    socket.on("connect_login", (msg) => {
-        console.log('Nueva conexión: '.green + socket.id.blue + ": " + msg.yellow)
-        clients.push({name: msg, id: socket.id})
-        socket.broadcast.emit("message",JSON.stringify(["general","server", "Se ha conectado: " + msg]));
+    socket.on('connect_login', (name) => {
+        console.log('Nueva conexión: '.green + socket.id.blue + ": " + name.yellow);
+        clients.push({ name, id: socket.id });
+        socket.broadcast.emit("message", JSON.stringify(["general", "server", `Se ha conectado: ${name}`]));
         io.emit("chatList", JSON.stringify(clients));
-        const fechaActual = new Date();
-        const hora = Number(fechaActual.getHours().toString().padStart(2, "0"));
-        socket.emit("message", JSON.stringify(["general", "server", "Hola " + msg + ", bienvenido."]));
-    });
-
-    socket.on('disconnect', function(){
-        console.log('CONEXIÓN TERMINADA CON: '.red + socket.id.yellow);
-        filtered_clients = []
-        for(let i = 0; i < clients.length; i++){
-            if(clients[i].id == socket.id){
-                io.emit("message", JSON.stringify([ "general", "server" ,"Se ha desconectado  " + clients[i].name ,"disconect" ,socket.id]));
-            }else{
-                filtered_clients.push(clients[i])
-            }
+        socket.emit("message", JSON.stringify(["general", "server", saludoHora() + name + ", bienvenido."]));
+        if (win) {
+            win.webContents.send('usersCon', clients);
+            win.webContents.send('genChat', ["general", "server", `Se ha conectado: ${name}`]);
         }
-        clients = filtered_clients
-        io.emit("chatList", JSON.stringify(clients));
     });
 
-    socket.on("message", (msg) => {
-        showMesageData(msg, socket.id)
-        if(msg[2][0] == "/"){
-            spetialCommands(msg[2], socket, msg[1], msg[0])
-        }else{
-            if(msg[0] == "general"){
+    socket.on('disconnect', () => {
+        console.log('CONEXIÓN TERMINADA CON: '.red + socket.id.yellow);
+        let nombre = '';
+        clients = clients.filter(c => {
+            if (c.id === socket.id) {
+                nombre = c.name;
+                io.emit("message", JSON.stringify(["general", "server", `Se ha desconectado  ${nombre}`, "disconect", socket.id]));
+                if (win) win.webContents.send('genChat', ["general", "server", `Se ha desconectado: ${nombre}`]);
+                return false;
+            }
+            return true;
+        });
+        io.emit("chatList", JSON.stringify(clients));
+        if (win) win.webContents.send('usersCon', clients);
+    });
+
+    socket.on('message', (msg) => {
+        showMesageData(msg, socket.id);
+        if (msg[2]?.[0] === "/") {
+            spetialCommands(msg[2], socket, msg[1], msg[0]);
+        } else {
+            if (msg[0] === "general") {
                 socket.broadcast.emit("message", JSON.stringify(msg));
-            }else{
-                destinatary = msg[0]
-                msg[0] = socket.id
+                if (win) win.webContents.send('genChat', msg);
+            } else {
+                const destinatary = msg[0];
+                msg[0] = socket.id;
                 io.to(destinatary).emit('message', JSON.stringify(msg));
             }
         }
     });
 });
 
-server.listen(PUERTO);
-console.log("Escuchando en puerto: ".yellow + String(PUERTO).blue);
+httpServer.listen(PUERTO, () =>
+    console.log("Escuchando en puerto: ".yellow + String(PUERTO).blue)
+);
 
-function spetialCommands(comand, socket, name, channel){
-    switch(comand){
+// --- FUNCIONES DE SERVIDOR ---
+
+function spetialCommands(command, socket, name, channel) {
+    switch (command) {
         case "/help":
-            socket.emit("message" ,JSON.stringify([channel ,"server","Comandos Disponibles: <br> - /list: Devuelve una lista con los usuarios conectados" + 
-            "<br> - /hello : Devuelve el saludo <br> - /date : Da la fecha actual "]))
+            socket.emit("message", JSON.stringify([channel, "server",
+                "Comandos Disponibles: <br> - /list: Ver usuarios conectados" +
+                "<br> - /hello: Devuelve el saludo<br> - /date: Da la fecha actual"
+            ]));
             break;
-
         case "/list":
-            let response = "Lista de usuarios conectados: "
-            if(clients.length <= 1){
-                response = "No hay nadie más conectado."
-            }else{
-                for(let i = 0; i < clients.length; i++){
-                    if(socket.id != clients[i].id){
-                        response += "<br> - " + clients[i].name
-                    }
-                }
-            }
-            socket.emit("message" ,JSON.stringify([channel ,"server",response]))
+            const otros = clients.filter(c => c.id !== socket.id);
+            const lista = otros.length > 0
+                ? otros.map(c => "- " + c.name).join("<br>")
+                : "No hay nadie más conectado.";
+            socket.emit("message", JSON.stringify([channel, "server", "Usuarios conectados:<br>" + lista]));
             break;
-
         case "/hello":
-            socket.emit("message", JSON.stringify([channel , "server", "Hola " + name + " !"]))
+            socket.emit("message", JSON.stringify([channel, "server", "Hola " + name + " !"]));
             break;
-
         case "/date":
-            socket.emit("message", JSON.stringify([channel , "server", getDate()]))
+            socket.emit("message", JSON.stringify([channel, "server", getDate()]));
             break;
-
         default:
-            socket.emit("message" , JSON.stringify([channel ,"server","Comando no reconocido, escribe /help para conocer todas las opciones"]))
-            break;
-    }    
-}
-
-function getDate(){
-    const fechaActual = new Date();
-    const dia = fechaActual.getDate().toString().padStart(2, '0');
-    const mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
-    const anio = fechaActual.getFullYear();
-    const hora = fechaActual.getHours().toString().padStart(2, '0');
-    const minutos = fechaActual.getMinutes().toString().padStart(2, '0');
-    const segundos = fechaActual.getSeconds().toString().padStart(2, '0');
-
-    const fechaHora = `Es el dia: ${dia}/${mes}/${anio} , a las ${hora}:${minutos} y ${segundos} segundos`;
-    return fechaHora
-}
-
-function showMesageData(msg, id){
-    console.log("________________________________________________".white)
-    console.log("Mensaje recibido: ".magenta)
-    console.log("origin id: ".blue + id.yellow)
-    console.log("Destination id: ".blue + msg[0].yellow)
-    if(msg[0] == "general"){
-        console.log("Message content: ".blue + msg[2].yellow)
-    }else{
-        console.log("Message content: ".blue + "PRIVATE CONVERSATION")
+            socket.emit("message", JSON.stringify([channel, "server", "Comando no reconocido. Usa /help para ver las opciones."]));
     }
-    console.log("________________________________________________".white)
 }
+
+function getDate() {
+    const f = new Date();
+    return `Es el día: ${f.getDate().toString().padStart(2, '0')}/${(f.getMonth() + 1).toString().padStart(2, '0')}/${f.getFullYear()} , a las ${f.getHours().toString().padStart(2, '0')}:${f.getMinutes().toString().padStart(2, '0')} y ${f.getSeconds().toString().padStart(2, '0')} segundos`;
+}
+
+function showMesageData(msg, id) {
+    console.log("________________________________________________".white);
+    console.log("Mensaje recibido: ".magenta);
+    console.log("origin id: ".blue + id.yellow);
+    console.log("Destination id: ".blue + msg[0].yellow);
+    console.log("Message content: ".blue + (msg[0] === "general" ? msg[2].yellow : "PRIVATE CONVERSATION"));
+    console.log("________________________________________________".white);
+}
+
+function saludoHora() {
+    const h = new Date().getHours();
+    if (h > 6 && h <= 13) return "Buenos días. ";
+    if (h > 13 && h <= 21) return "Buenas tardes. ";
+    return "Buenas noches. ";
+}
+
+// --- ELECTRON WINDOW (GUI) ---
+app.whenReady().then(() => {
+    win = new BrowserWindow({
+        width: 1200,
+        height: 750,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile("GUI/renderer.html");
+    win.setMenuBarVisibility(false);
+
+    win.once('ready-to-show', () => {
+        const url = "http://" + ip.address() + ":" + PUERTO;
+        const qrPath = "./GUI/QR.png";
+
+        qrcode.toFile(qrPath, url, {
+            color: { dark: '#000000', light: '#0000' }
+        }, (err) => {
+            if (!err) {
+                console.log('--> New QR made <--'.yellow);
+                win.webContents.send('QR', "OK");
+            }
+        });
+
+        win.webContents.send('systemInfo', {
+            node: process.versions.node,
+            chrome: process.versions.chrome,
+            electron: process.versions.electron,
+            ip: ip.address(),
+            port: PUERTO
+        });
+
+        win.webContents.send('conectionInformation', JSON.stringify([ip.address(), PUERTO, "QR.png"]));
+        win.webContents.send('usersCon', clients);
+    });
+
+    ipcMain.handle('serverMess', (_, msg) => {
+        io.emit("message", JSON.stringify(["general", "server", msg]));
+    });
+});
